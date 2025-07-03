3348a4c20abc350dbd733aecbb733edb87e42b55
xeon <eye.debashis@gmail.com>
2025-07-03 07:26:12 +0600

    Initial completed project commit

---
+++ Assets/LeanTouch/OtherScript/Rotate.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotate : MonoBehaviour {
+	public Bounds bounds;
+	public float speed = 1.0F;
+	// Use this for initialization
+	void Start () {
+		var collider = gameObject.GetComponent<BoxCollider> ();
+		if (collider == null) {
+			collider = gameObject.AddComponent<BoxCollider>();
+			Debug.Log("No Collider is Detected");
+		}
+		bounds = collider.bounds;
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if(Input.GetMouseButton(0)){
+			if(bounds.size.magnitude > 0){
+				var dtx = Input.GetAxis("Mouse X")*speed;
+				var dty = Input.GetAxis("Mouse Y")*speed;
+				var pivot = bounds.center;
+
+				transform.RotateAround(pivot,Vector3.up,dtx);
+				transform.RotateAround(pivot,Vector3.left,dty);
+				transform.Rotate(Vector3.up * Time.deltaTime * speed);
+			}
+		}
+	}
+}
---
+++ Assets/LeanTouch/OtherScript/SpinWithTouch.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class SpinWithTouch : MonoBehaviour
+{
+
+	void Start()
+	{
+		
+	}
+
+    public void OnMouseDrag()
+    {
+        if (Input.touchCount > 0)
+        {
+            if (EventSystem.current.IsPointerOverGameObject(Input.GetTouch(0).fingerId))
+            {
+                return;
+            }
+            else
+            {
+                
+                    var ray = Camera.main.ScreenPointToRay(Input.touches[0].position);
+                    var hitInfo = new RaycastHit();
+                    float rotSpeed = 20;
+                    if (Physics.Raycast(ray, out hitInfo))
+                    {
+                        if (hitInfo.transform.name != transform.name)
+                            return;
+                        float rotX = Input.GetAxis("Mouse X") * rotSpeed * Mathf.Deg2Rad;
+                        transform.Rotate(Vector3.up, -rotX);
+
+                        //var yAxis = hitInfo.point.y*rotSpeed;
+                        //transform.Rotate(Vector3.up, yAxis);
+                    }
+                
+            }
+        }
+        Debug.Log("Touched");
+    }
+}
